bae5ecf1de31995ac2bb8e1a9dda18c15233e610
NaniToy <yulianbar2681@gmail.com>
2022-12-28 18:24:22 +0100

    Add file

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ class Кошеня:
 
     def харчуватись(self):
         pass
+    def шипіти(self):
+        pass
+class Собака:
+    def пити_воду(self):
+        pass
+    def дихати(self):
+        pass
+
+    def рухатись(self):
+        pass
+
+    def харчуватись(self):
+        pass
+    def гавкати(self):
+        pass
